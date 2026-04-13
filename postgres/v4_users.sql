485335c502451b52f621b9072fab089d9838ccbb
lnhacanhaca <luis.jnhacanhaca@gmail.com>
2026-04-13 14:28:47 +0200

    Fix bcrypt password hash for test accounts

--- postgres/v4_users.sql
+++ postgres/v4_users.sql
@@ -10,5 +10,5 @@ DELETE FROM usuarios WHERE email IN ('editor@teu.bot', 'leitor@teu.bot');
 -- 3. Inserir a conta de Editor e Leitor com senha = "123"
 INSERT INTO usuarios (nome, email, senha, nivel_acesso) 
 VALUES 
-('Equipa Edição', 'editor@teu.bot', '$2b$10$eE/.C7yHOnwO00MAlXQZtOuF7G2K.UToS/CgB43s7zXy8Q8yXoBwO', 'editor'),
-('Visitante Stats', 'leitor@teu.bot', '$2b$10$eE/.C7yHOnwO00MAlXQZtOuF7G2K.UToS/CgB43s7zXy8Q8yXoBwO', 'leitor');
+('Equipa Edição', 'editor@teu.bot', '$2b$10$19HL81ouhvL7Qhecj6NN2Odshup4XUYJ1JbFJ9lVoE3/Ey/uEMcH6', 'editor'),
+('Visitante Stats', 'leitor@teu.bot', '$2b$10$19HL81ouhvL7Qhecj6NN2Odshup4XUYJ1JbFJ9lVoE3/Ey/uEMcH6', 'leitor');
